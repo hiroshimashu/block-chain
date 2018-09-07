88314df2b35cbb4239b95c0a7f79bed6830fe711
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-07 21:59:51 +0900

    add methods to implement new transction

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,39 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
     
-    def new_block(self):
-        # Creates a new Block and add it to the chain
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+
     
     def new_transaction(self, sender, recipient, amount):
         """
@@ -21,17 +49,21 @@ class Blockchain(object):
             'amount':amount,
         })
         return self.last_block['index'] + 1
-
    
     @staticmethod
     def hash(block):
-        # Hashes a Block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        block_string = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(block_string).hexdigets()
     
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
     
     
 
