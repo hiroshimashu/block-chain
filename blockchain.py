d07d5f4e2195f56887a2536501d1ffd783168bb0
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-09-10 15:56:24 +0900

    add proof_of_work

--- blockchain.py
+++ blockchain.py
@@ -49,7 +49,26 @@ class Blockchain(object):
             'amount':amount,
         })
         return self.last_block['index'] + 1
-   
+    
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+           proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigets()
+        return guess_hash[:4] == "0000"
+
     @staticmethod
     def hash(block):
         """
